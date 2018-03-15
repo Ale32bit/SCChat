6680b48f5aa866b4aef3c2d2b916afdfb7fe05cd
Ale32bit <ale.proto00@gmail.com>
2018-03-15 16:49:20 +0100

    Added time, icon, reconnect, clickable urls, emojis for special events, console messages support

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const electron = require('electron');
 const {app, BrowserWindow} = electron;
 const path = require('path');
 const url = require('url');
+const open = require("open");
 let mainWindow;
 function createWindow () {
     mainWindow = new BrowserWindow({
-        width: 600,
-        height: 800
+        width: 1000,
+        height: 800,
+        icon: __dirname + '/icon.ico',
     });
+
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
+
+    mainWindow.webContents.on('new-window', function(e, url) {
+        e.preventDefault();
+        open(url)
+    });
 
     mainWindow.on('close', function () {
         app.quit();
